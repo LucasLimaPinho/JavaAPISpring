07b97c4801c5df9137b6199bfa0e08cda431e327
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-08 18:56:57 -0300

    Utilizando o @Primary para diferenciar os beans em um @Autowired

--- src/main/java/com/claro/spring/UR4/test/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/claro/spring/UR4/test/springin5steps/QuickSortAlgorithm.java
@@ -1,8 +1,23 @@
 package com.claro.spring.UR4.test.springin5steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 
+//Se eu definir dois @Component para o @Autorwired SortAlgorithm teremos o seguinte erro em
+// logging.level.com.springframework = debug
+
+
+/* Parameter 0 of constructor in com.claro.spring.UR4.test.springin5steps.BinarySearchImpl required a single bean, but 2 were found:
+- bubbleSortAlgorithm: defined in file [C:\Users\Semantix\spring-learning\spring-in-5-steps\target\classes\com\claro\spring\UR4\test\springin5steps\BubbleSortAlgorithm.class]
+- quickSortAlgorithm: defined in file [C:\Users\Semantix\spring-learning\spring-in-5-steps\target\classes\com\claro\spring\UR4\test\springin5steps\QuickSortAlgorithm.class]
+*/
+
+// A forma de contornar isso é fazendo o add primary.
+// Usando @Primary
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 	
 	public int [] sort(int[] numbers) {
